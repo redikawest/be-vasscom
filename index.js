9066c1067e2ffb6ed13b0ca16882722bfe61135c
Redika Westama Putra <redikawest@Redikas-MacBook-Air-2.local>
2024-01-19 14:16:25 +0800

    fix cors

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const productRoutes = require('./routes/product');
 const userRoutes = require('./routes/user');
 const authRoutes = require('./routes/auth');
 const app = express()
+const cors = require('cors')
 const port = 3000
 
 app.use(express.json());
+app.use(cors())
+
 app.use('/api', productRoutes);
 app.use('/api', userRoutes);
 app.use('/api', authRoutes);
